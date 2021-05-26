e83cc5bf0de5893acbb8c9beb6984ecb4de6c4b9
friendbear <friendbear@users.noreply.github.com>
2021-05-26 14:10:02 +0900

    文字の代入

--- src/basic/str.py
+++ src/basic/str.py
@@ -61,3 +61,25 @@ print(s2.title())
 print(s2.upper())
 print(s2.lower())
 print(s2.replace('Mike', 'Nancy'))
+
+## 文字の代入
+print('a is {}'.format('test'))
+print('a is {} {} {}'.format(1, 2, 3))
+print('a is {2} {1} {0}'.format(1, 2, 3))
+
+print('My name is {0} {1}'.format('Jun', 'Sakai'))
+
+print('My name is {name} {family}. Watashi ha {family} {name}'.format(name='Jun', family='Sakai'))
+
+
+## f-strings
+a = 'a'
+print(f'a is {a}')
+
+x, y, z = 1, 2, 3
+print(f'a is {x}, {y}, {z}')
+print(f'a is {z}, {y}, {x}')
+     
+name = 'Jun'
+family = 'Sakai'
+print(f'My name is {name} {family}. Watashi ha {family} {name}')
