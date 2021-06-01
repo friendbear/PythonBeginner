d96825ad6f5375e9e3e7ef4abc23d33b2b7d8c8f
friendbear <friendbear@users.noreply.github.com>
2021-06-01 17:52:18 +0900

    tuple cont.

--- src/structure/tuple.py
+++ src/structure/tuple.py
@@ -25,3 +25,36 @@ new_tuple = (1, 2, 3) + (2, 3, 4)
 print(new_tuple)
 
 
+## unpacking
+
+num_tuple = (10, 20)
+print(new_tuple)
+
+x, y = num_tuple
+print(x, y)
+
+min, max = 0, 100
+print(min, max)
+
+
+i = 10
+j = 20
+tmp = i
+i = j 
+j = tmp
+print(i, j)
+
+a = 100
+b = 200
+print(a, b)
+b, a = a, b
+print(a, b)
+
+##
+chose_from_two = ( 'A', 'B', 'C')
+
+answer = []
+answer.append('A')
+answer.append('B')
+print(chose_from_two)
+print(answer)
