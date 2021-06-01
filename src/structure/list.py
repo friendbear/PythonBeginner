ec9afa2cdf791c913b23bd738dadb57207f706d9
friendbear <friendbear@users.noreply.github.com>
2021-06-01 16:12:12 +0900

    list methods

--- src/structure/list.py
+++ src/structure/list.py
@@ -36,3 +36,22 @@ print(n)
 
 n.insert(0, 200)
 print(n)
+
+r = [1, 2, 3, 4, 5, 1, 2, 3]
+print(r.index(3, 3))
+
+if 5 in r:
+    print('exist')
+
+r.sort()
+print(r)
+r.sort(reverse=True)
+print(r)
+r.reverse()
+print(r)
+
+
+s = 'My name is Mike.'
+to_split = s.split(' ')
+x = ' ######'.join(to_split)
+print(x)
