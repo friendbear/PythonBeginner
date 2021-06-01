675466803ff898f8f176eff5b559ade492714909
friendbear <friendbear@users.noreply.github.com>
2021-06-01 16:43:16 +0900

    tuple

---
+++ src/structure/tuple.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+t = (1, 2, 3, 4, 5, 1, 2)
+
+print(t)
+
+type(t)
+
+print(t[0])
+print(t[-1])
+
+print(t[2:5])
+print(t.index(1))
+
+print(t.index(1, 1))
+print(t.count(1))
+
+t = ([1, 2, 3], [4, 5, 6])
+
+print(t[0][0])
+
+new_tuple = (1, 2, 3) + (2, 3, 4)
+
+print(new_tuple)
+
+
