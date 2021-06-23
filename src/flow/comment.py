f441149adb992f06869ef63d5407de11bbdc7b2c
friendbear <friendbear@users.noreply.github.com>
2021-06-23 14:23:43 +0900

    some comments

---
+++ src/flow/comment.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+s = 'aaaaaaaaaaaaaaaaaaaa' \
+    + 'bbbbbbbbbbbbbbbbbbb'
+print(s)
+
+x = 1 + 1 + 1 + 1 + 1 + 1 + \
+    1 + 1 + 1 + 1 + 1 + 1 + 1 + 1
+print(x)
+y = (1 + 1 + 1 + 1 + 1 + 1 + 
+    1 + 1 + 1 + 1 + 1 + 1 + 1 + 1)
+print(y)
