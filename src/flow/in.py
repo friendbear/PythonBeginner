1940cb5eb7119ad5ee4c5c57775c6a4152764b35
friendbear <friendbear@users.noreply.github.com>
2021-06-23 14:39:42 +0900

    in and not in

---
+++ src/flow/in.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+y = [1, 2, 3]
+x = 1
+
+if x in y:
+    print('in')
+
+if 100 not in y:
+    print('not in')
+
+a = 1
+b = 2
+
+if not a == b:
+    print('Not equal')
+
+is_ok = True
+if is_ok:
+    print('hello')
+if not is_ok:
+    print('hello')
