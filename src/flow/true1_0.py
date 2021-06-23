5b552e6608ca456beba350e03c1e7f989472a86f
friendbear <friendbear@users.noreply.github.com>
2021-06-23 14:40:12 +0900

    True, False example

---
+++ src/flow/true1_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+is_ok = True
+is_ok = 1 # Number, Empty string => False
+
+is_ok = [] # Empty List => False
+is_ok = set() # Empty set => False
+
+if is_ok:
+    print('OK!')
+else:
+    print('No!')
