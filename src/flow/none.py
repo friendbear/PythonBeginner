61bf4149e48b2174f55ef38ebe6cf5d760fd65c3
friendbear <friendbear@users.noreply.github.com>
2021-06-23 15:05:15 +0900

    None

---
+++ src/flow/none.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+is_empty = None
+print(is_empty)
+
+if is_empty is not None:
+    print('None!!!')
+
+print(1 == True)
+print(1 is True) # NG
+
+print(None is None)
