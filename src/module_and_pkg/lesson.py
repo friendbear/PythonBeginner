1242938478e8a4c08ccf1c89bb593f84739af4d8
friendbear <friendbear@users.noreply.github.com>
2022-02-18 10:59:12 +0900

    絶対パスと相対パスのImpoort

--- src/module_and_pkg/lesson.py
+++ src/module_and_pkg/lesson.py
@@ -1,13 +1,19 @@
 import sys
 #import lesson_package.utils  # フルパスで書くとモジュールの場所がわかりやすい
-from lesson_package import utils # 一般的に推奨
+from src.module_and_pkg.lesson_package.tools import utils
+
 #from lesson_package import utils as u # 一般的に好まれない
 # from lesson_package.utils import say_twice
 
 # lesson_packageの関数呼び出し
 r = utils.say_twice('hello')
-
 print(r)
+
+from lesson_package.talk import human
+# talkのモジュールの関数呼び出し
+print(human.sing())
+
+
 print(sys.argv)
 
 for i in sys.argv:
---
+++ src/module_and_pkg/lesson_package/talk/human.py
@@ -0,0 +1,12 @@
+from src.module_and_pkg.lesson_package.talk import human
+from ..tools import utils
+def sing():
+    return 'sing'
+
+def cry():
+    return 'wow!'
+
+print(human.sing())
+print(human.cry())
+
+print()
