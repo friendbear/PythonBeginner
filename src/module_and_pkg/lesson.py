9c438a95f074b516c79561cb044b715cb955534f
friendbear <friendbear@users.noreply.github.com>
2022-02-18 10:43:01 +0900

    Import文とAS

--- src/module_and_pkg/lesson.py
+++ src/module_and_pkg/lesson.py
@@ -1,6 +1,15 @@
 import sys
+#import lesson_package.utils  # フルパスで書くとモジュールの場所がわかりやすい
+from lesson_package import utils # 一般的に推奨
+#from lesson_package import utils as u # 一般的に好まれない
+# from lesson_package.utils import say_twice
 
+# lesson_packageの関数呼び出し
+r = utils.say_twice('hello')
+
+print(r)
 print(sys.argv)
 
 for i in sys.argv:
     print(i)
+
