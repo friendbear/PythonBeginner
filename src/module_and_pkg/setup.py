6bc8c222e0201cfbe391cf2376a4868c2f1b15a1
friendbear <friendbear@users.noreply.github.com>
2022-02-21 13:17:11 +0900

    python setup.py sdist

---
+++ src/module_and_pkg/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='python-beginner',
+    version='1.0.0',
+    packages=['lesson_package',
+              'lesson_package.talk', 'lesson_package.tools'],
+    url='',
+    license='free',
+    author='k22',
+    author_email='',
+    description='Sample package'
+)
